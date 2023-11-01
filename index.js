46bd0cde83e89f800570847e828095be1ca6d4e1
GreteGroenner <ggroenner@gmail.com>
2023-11-01 17:38:48 +0100

    Html, css and tiny bit js
    Changes to html nav items and added first bit of js in mobile view

---
+++ index.js
@@ -0,0 +1,18 @@
+function openHamburgerMenu() {  
+    let x = document.getElementById("mobile-nav");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  } 
+
+  function closeHamburgerMenu() {
+   
+    let x = document.getElementById("mobile-nav");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  }
